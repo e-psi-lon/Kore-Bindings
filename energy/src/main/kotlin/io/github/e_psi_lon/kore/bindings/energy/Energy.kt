48653f3df6a9cd71549ef16b28eadc8e0f4ac16a
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-06 20:31:02 +0200

    refactor(energy): Use auto-generated code instead of manual code

--- energy/src/main/kotlin/io/github/e_psi_lon/kore/bindings/energy/Energy.kt
+++ energy/src/main/kotlin/io/github/e_psi_lon/kore/bindings/energy/Energy.kt
@@ -5,146 +5,115 @@ import io.github.ayfri.kore.arguments.scores.Scores
 import io.github.ayfri.kore.arguments.scores.SelectorScore
 import io.github.ayfri.kore.arguments.scores.score
 import io.github.ayfri.kore.arguments.types.literals.SelectorArgument
-import io.github.ayfri.kore.commands.function
-import io.github.ayfri.kore.functions.Function
-import io.github.e_psi_lon.kore.bindings.core.Library
-import io.github.e_psi_lon.kore.bindings.core.SupportedSource
-import io.github.e_psi_lon.kore.bindings.lantern_load.LanternLoad
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-object Energy : Library {
-    override val namespace: String = "energy"
-    override val version: String
-        get() = ""
-    override val source: SupportedSource
-        get() = SupportedSource.DOWNLOAD
-    override val location: String
-        get() = ""
-    override val externalDependencies: List<Library>
-        get() = listOf(LanternLoad)
 
-    val api
-        get() = Api
-
-    fun send(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
-        selector.selector.nbtData.apply {
-            tag = if (reverse) {
-                "$namespace.send"
-            } else {
-                !"$namespace.send"
-            }
+fun Energy.send(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
+    selector.selector.nbtData.apply {
+        tag = if (reverse) {
+            "$namespace.send"
+        } else {
+            !"$namespace.send"
         }
-        return selector
     }
-
-    fun sender(selector: SelectorArgument): SelectorArgument {
-        return receive(send(selector), true)
-    }
-
-    fun receive(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
-        selector.selector.nbtData.apply {
-            tag = if (reverse) {
-                "$namespace.receive"
-            } else {
-                !"$namespace.receive"
-            }
-        }
-        return selector
-    }
-
-    fun receiver(selector: SelectorArgument): SelectorArgument {
-        return receive(send(selector, true))
-    }
-
-    fun cable(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
-        selector.selector.nbtData.apply {
-            tag = if (reverse) {
-                "$namespace.cable"
-            } else {
-                !"$namespace.cable"
-            }
-        }
-        return selector
-    }
-
-    fun senderAndReceiver(selector: SelectorArgument): SelectorArgument {
-        return send(receive(selector))
-    }
-
-    context(Function)
-    fun cableCanConnect() = function(namespace, "v1/cable_can_connect", true)
-
-    context(Function)
-    fun cableUpdate() = function(namespace, "v1/cable_update", true)
-
-    context(Function)
-    fun energyUpdate() = function(namespace, "v1/energy_update", true)
-
-    context(Function)
-    fun updateEnergyItem() = function("namespace, v1/update_energy_item", true)
-
-    @Serializable
-    @SerialName("energy")
-    data class EnergyData(val storage: Int, val maxStorage: Int)
-
-    context(Scores<SelectorScore>)
-    fun data() = score("$namespace.data")
-
-    context(Scores<SelectorScore>)
-    fun data(value: Int) = score("$namespace.data", value)
-
-    context(Scores<SelectorScore>)
-    fun data(value: IntRange) = score("$namespace.data", value)
-
-    context(Scores<SelectorScore>)
-    fun data(value: IntRangeOrInt) = score("$namespace.data", value)
-
-    context(Scores<SelectorScore>)
-    fun storage() = score("$namespace.storage")
-
-    context(Scores<SelectorScore>)
-    fun storage(value: Int) = score("$namespace.storage", value)
-
-    context(Scores<SelectorScore>)
-    fun storage(value: IntRange) = score("$namespace.storage", value)
-
-    context(Scores<SelectorScore>)
-    fun storage(value: IntRangeOrInt) = score("$namespace.storage", value)
-
-    context(Scores<SelectorScore>)
-    fun maxStorage() = score("$namespace.max_storage")
-
-    context(Scores<SelectorScore>)
-    fun maxStorage(value: Int) = score("$namespace.max_storage", value)
-
-    context(Scores<SelectorScore>)
-    fun maxStorage(value: IntRange) = score("$namespace.max_storage", value)
-
-    context(Scores<SelectorScore>)
-    fun maxStorage(value: IntRangeOrInt) = score("$namespace.max_storage", value)
-
-    context(Scores<SelectorScore>)
-    fun transferRate() = score("$namespace.transfer_rate")
-
-    context(Scores<SelectorScore>)
-    fun transferRate(value: Int) = score("$namespace.transfer_rate", value)
-
-    context(Scores<SelectorScore>)
-    fun transferRate(value: IntRange) = score("$namespace.transfer_rate", value)
-
-    context(Scores<SelectorScore>)
-    fun transferRate(value: IntRangeOrInt) = score("$namespace.transfer_rate", value)
-
-    context(Scores<SelectorScore>)
-    fun changeRate() = score("$namespace.change_rate")
-
-    context(Scores<SelectorScore>)
-    fun changeRate(value: Int) = score("$namespace.change_rate", value)
-
-    context(Scores<SelectorScore>)
-    fun changeRate(value: IntRange) = score("$namespace.change_rate", value)
-
-    context(Scores<SelectorScore>)
-    fun changeRate(value: IntRangeOrInt) = score("$namespace.change_rate", value)
+    return selector
 }
+
+fun Energy.sender(selector: SelectorArgument): SelectorArgument {
+    return receive(send(selector), true)
+}
+
+fun Energy.receive(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
+    selector.selector.nbtData.apply {
+        tag = if (reverse) {
+            "$namespace.receive"
+        } else {
+            !"$namespace.receive"
+        }
+    }
+    return selector
+}
+
+fun Energy.receiver(selector: SelectorArgument): SelectorArgument {
+    return receive(send(selector, true))
+}
+
+fun Energy.cable(selector: SelectorArgument, reverse: Boolean = false): SelectorArgument {
+    selector.selector.nbtData.apply {
+        tag = if (reverse) {
+            "$namespace.cable"
+        } else {
+            !"$namespace.cable"
+        }
+    }
+    return selector
+}
+
+fun Energy.senderAndReceiver(selector: SelectorArgument): SelectorArgument {
+    return send(receive(selector))
+}
+
+@Serializable
+@SerialName("energy")
+data class EnergyData(val storage: Int, val maxStorage: Int)
+
+context(Scores<SelectorScore>)
+fun Energy.data() = score("$namespace.data")
+
+context(Scores<SelectorScore>)
+fun Energy.data(value: Int) = score("$namespace.data", value)
+
+context(Scores<SelectorScore>)
+fun Energy.data(value: IntRange) = score("$namespace.data", value)
+
+context(Scores<SelectorScore>)
+fun Energy.data(value: IntRangeOrInt) = score("$namespace.data", value)
+
+context(Scores<SelectorScore>)
+fun Energy.storage() = score("$namespace.storage")
+
+context(Scores<SelectorScore>)
+fun Energy.storage(value: Int) = score("$namespace.storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.storage(value: IntRange) = score("$namespace.storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.storage(value: IntRangeOrInt) = score("$namespace.storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.maxStorage() = score("$namespace.max_storage")
+
+context(Scores<SelectorScore>)
+fun Energy.maxStorage(value: Int) = score("$namespace.max_storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.maxStorage(value: IntRange) = score("$namespace.max_storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.maxStorage(value: IntRangeOrInt) = score("$namespace.max_storage", value)
+
+context(Scores<SelectorScore>)
+fun Energy.transferRate() = score("$namespace.transfer_rate")
+
+context(Scores<SelectorScore>)
+fun Energy.transferRate(value: Int) = score("$namespace.transfer_rate", value)
+
+context(Scores<SelectorScore>)
+fun Energy.transferRate(value: IntRange) = score("$namespace.transfer_rate", value)
+
+context(Scores<SelectorScore>)
+fun Energy.transferRate(value: IntRangeOrInt) = score("$namespace.transfer_rate", value)
+
+context(Scores<SelectorScore>)
+fun Energy.changeRate() = score("$namespace.change_rate")
+
+context(Scores<SelectorScore>)
+fun Energy.changeRate(value: Int) = score("$namespace.change_rate", value)
+
+context(Scores<SelectorScore>)
+fun Energy.changeRate(value: IntRange) = score("$namespace.change_rate", value)
+
+context(Scores<SelectorScore>)
+fun Energy.changeRate(value: IntRangeOrInt) = score("$namespace.change_rate", value)
