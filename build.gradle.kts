c6fa72295e2d5cca295b7b118c69aa7399f2ac85
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-06 20:34:21 +0200

    build: Add a helper task to automatically generate bindings locally

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,16 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+// Add a task that runs all "generateBindings" tasks. Notice that some subprojects doesn't have this task.
+tasks.register("generateAllBindings") {
+    group = "build"
+    description = "Runs all generateBindings tasks."
+
+    subprojects.forEach { subproject ->
+        val generateBindingsTask = subproject.tasks.findByName("generateBindings")
+        if (generateBindingsTask != null) {
+            dependsOn(generateBindingsTask)
+        }
+    }
+}
