3f369fe441b4f086855e1c2b4bd68c6a2ae666fc
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-10-13 21:50:19 +0200

    fix(Logger): Reorder checks for clarity and validate Gradle logger initialization

--- generation/src/main/kotlin/io/github/e_psi_lon/kore/bindings/generation/Logger.kt
+++ generation/src/main/kotlin/io/github/e_psi_lon/kore/bindings/generation/Logger.kt
@@ -12,26 +12,30 @@ enum class Level {
  * Minimal abstraction allowing to switch between Gradle's logger and stdout/stderr
  */
 class Logger(private val useGradleLogger: Boolean, private val gradleLogger: GradleLogger? = null, val level: Level = Level.INFO) {
+
+    init {
+        require(!useGradleLogger || gradleLogger != null) { "Gradle logger must be provided if useGradleLogger is true" }
+    }
     fun info(message: String) {
-        if (Level.INFO < level) return
+        if (level > Level.INFO) return
         if (useGradleLogger && gradleLogger != null) gradleLogger.lifecycle(message)
         else println("[INFO] $message")
     }
 
     fun warn(message: String) {
-        if (Level.WARN < level) return
+        if (level > Level.WARN) return
         if (useGradleLogger && gradleLogger != null) gradleLogger.warn(message)
         else println("[WARN] $message")
     }
 
     fun error(message: String) {
-        if (Level.ERROR < level) return
+        if (level > Level.ERROR) return
         if (useGradleLogger && gradleLogger != null) gradleLogger.error(message)
         else System.err.println("[ERROR] $message")
     }
 
     fun <T: Exception> error(message: String, exception: T): T {
-        if (Level.ERROR < level) return exception
+        if (level > Level.ERROR) return exception
         if (useGradleLogger && gradleLogger != null) gradleLogger.error(message, exception)
         else {
             System.err.println("[ERROR] $message")
@@ -41,13 +45,13 @@ class Logger(private val useGradleLogger: Boolean, private val gradleLogger: Gra
     }
 
     fun debug(message: String) {
-        if (Level.DEBUG < level) return
+        if (level > Level.DEBUG) return
         if (useGradleLogger && gradleLogger != null) gradleLogger.debug(message)
         else println("[DEBUG] $message")
     }
 
     fun trace(message: String) {
-        if (Level.TRACE < level) return
+        if (level > Level.TRACE) return
         if (useGradleLogger && gradleLogger != null) gradleLogger.trace(message)
         else println("[TRACE] $message")
     }
