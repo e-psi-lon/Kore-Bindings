531a61fb44c89ea47004a89646250096553bc174
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-11-06 12:02:41 +0100

    refactor(generator-poet): Simplify the `addDocs` function and add `codeBlock` utility

--- generation/src/main/kotlin/io/github/e_psi_lon/kore/bindings/generation/poet/HelperBuilder.kt
+++ generation/src/main/kotlin/io/github/e_psi_lon/kore/bindings/generation/poet/HelperBuilder.kt
@@ -1,6 +1,7 @@
 package io.github.e_psi_lon.kore.bindings.generation.poet
 
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.MemberName.Companion.member
@@ -9,9 +10,8 @@ import com.squareup.kotlinpoet.typeNameOf
 import kotlin.reflect.KFunction
 import kotlin.reflect.jvm.javaMethod
 
-fun FunSpec.Builder.addDocs(vararg docs: String): FunSpec.Builder {
-    return addKdoc("%L", docs.joinToString("\n"))
-}
+fun FunSpec.Builder.addDocs(vararg docs: String): FunSpec.Builder =
+    addKdoc("%L", docs.joinToString("\n"))
 
 inline fun <reified T> FunSpec.Builder.addParameter(name: String, block: ParameterSpec.Builder.() -> Unit = {}): FunSpec.Builder {
     val builder = ParameterSpec.builder(name, typeNameOf<T>()).apply(block)
@@ -26,3 +26,7 @@ fun KFunction<*>.asMemberName(): MemberName {
     val className = ClassName(packageName, rawClassName.split('.'))
     return className.member(this.name)
 }
+
+
+fun codeBlock(builder: CodeBlock.Builder.() -> Unit) =
+    CodeBlock.builder().apply(builder).build()
